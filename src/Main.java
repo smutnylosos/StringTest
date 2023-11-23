b0e69b0e18dea3f294c9d146ad6ff9d35b4ee59c
Kacper <maustriacki@gmail.com>
2023-11-23 13:26:10 +0100

    StringTest

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
-public class Main {
+//Z ciągu znaków wytnij 2 środkowe litery i przerzuć je na początek
 
-}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        new string(str.ciagznakow);
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hello world");
+
+        for (str.ciagznakow < 3; i++;) {
+            System.out.println("podaj dłuższy ciąg znaków");
+
+        }
+
+
+
+
+        }
+
+    }
